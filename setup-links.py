e9b1e2a757a32b55ca4544cd5faa73d7fe91eb3e
Shea Craig <shea.craig@da.org>
2015-05-05 13:58:59 -0400

    Move powerline fonts to submodule.

--- setup-links.py
+++ setup-links.py
@@ -24,10 +24,10 @@ FORTUNE_PATH = '/usr/local/bin'
 FORTUNES = ['fortunes']
 FORTUNES_PATH = '/usr/local/var'
 
-def check_and_link(files, destination):
+
+def check_and_link(files, destination, backupd):
     """Check for files and move them to backup, then symlink."""
     for dotfile in files:
-        src = os.path.join(dotfilesd, dotfile)
         dst = os.path.join(destination, dotfile)
         if os.path.exists(dst):
             if os.path.islink(dst):
@@ -37,53 +37,75 @@ def check_and_link(files, destination):
                 print("File %s exists; copying to backup directory: %s" %
                     (dst, backupd))
                 shutil.move(dst, backupd)
-        os.symlink(src, dst)
-        print("Linked %s to %s" % (src, dst))
+
+        os.symlink(dotfile, dst)
+        print("Linked %s to %s" % (dotfile, dst))
 
 
-def ensure_directory(target, title=''):
+def ensure_directory(target, title=""):
+    """Make a directory if it doesn't already exist.
+
+    Args:
+        target: String path to a directory to ensure.
+        title: String name of software which should have made the dir.
+            Defaults to "". Useful as a warning about needing to set
+            up other projects.
+    """
     if not os.path.isdir(target):
         os.makedirs(target)
         if title:
-            print("Warning: %s not installed!" % title)
+            print "Warning: %s not installed!" % title
 
 
 def git_clone(url, into):
     subprocess.check_call(["git", "clone", url, into])
 
 
-if len(sys.argv) != 2:
-    if os.path.basename(os.getcwd()) == 'dotfiles':
-        dotfilesd = os.getcwd()
-    else:
-        sys.exit("Please provide path to dotfiles as an argument.")
-else:
-    dotfilesd = os.path.realpath(sys.argv[1])
+def git_submodule_init():
+    subprocess.check_call(["git", "submodule", "update", "--init"])
 
-print("Dotfiles directory: %s" % dotfilesd)
 
-# Make a backup directory.
-backupd = os.path.join(os.getcwd(), 'backup-%s' %
-                       time.strftime('%Y%m%d-%H%M%S'))
-os.mkdir(backupd)
+def install_powerline_fonts():
+    subprocess.check_call(["fonts/install.sh"])
 
-# Setup dotfiles in the home.
-check_and_link(HOME_DOTFILES, os.getenv("HOME"))
 
-# Setup luggage files.
-ensure_directory(LUGGAGE_PATH, 'Luggage')
-check_and_link(LUGGAGE_DOTFILES, LUGGAGE_PATH)
+def main():
+    """Set up each dotfile resource."""
+    # Make a backup directory.
+    backupd = os.path.join(os.getcwd(), "backup-%s" %
+                           time.strftime("%Y%m%d-%H%M%S"))
+    os.mkdir(backupd)
 
-# fortune, cowsay
-ensure_directory(FORTUNE_PATH)
-ensure_directory(FORTUNES_PATH)
-check_and_link(FORTUNE, FORTUNE_PATH)
-check_and_link(FORTUNES, FORTUNES_PATH)
+    # Get the location of the dotfiles and cd there.
+    dotfilesd = os.path.realpath(os.path.dirname(sys.argv[0]))
+    os.chdir(dotfilesd)
+    print("Dotfiles directory: %s" % dotfilesd)
 
-cowsay = glob.glob(os.path.join(dotfilesd, 'cowsay*.pkg'))[0]
-output = subprocess.check_output(['installer', '-target', '/', '-pkg', cowsay])
-print(output)
+    # Setup dotfiles in the home.
+    check_and_link(HOME_DOTFILES, os.getenv("HOME"), backupd)
 
-# Powerline fonts.
-git_clone("https://github.com/powerline/fonts", "/tmp/powerline-fonts")
-subprocess.check_call(["/tmp/powerline-fonts/install.sh"])
+    # Set up git submodules.
+    git_submodule_init()
+
+    # Install powerline fonts.
+    install_powerline_fonts()
+
+    # Setup luggage files.
+    ensure_directory(LUGGAGE_PATH, 'Luggage')
+    check_and_link(LUGGAGE_DOTFILES, LUGGAGE_PATH, backupd)
+
+    # fortune, cowsay
+    ensure_directory(FORTUNE_PATH)
+    ensure_directory(FORTUNES_PATH)
+    check_and_link(FORTUNE, FORTUNE_PATH, backupd)
+    check_and_link(FORTUNES, FORTUNES_PATH, backupd)
+
+    if  sys.platform == "darwin":
+        cowsay = glob.glob('cowsay*.pkg')[0]
+        output = subprocess.check_output(['installer', '-target', '/', '-pkg',
+                                          cowsay])
+        print(output)
+
+
+if __name__ == "__main__":
+    main()
