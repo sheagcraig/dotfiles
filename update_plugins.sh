f367b02a5a57fdbd75ed42da045a511361192407
Shea Craig <sheagcraig@gmail.com>
2015-11-08 20:56:51 -0500

    Add branches to .gitmodules, and update update script to use them.

--- update_plugins.sh
+++ update_plugins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Update vim submodules for updating the git dotfiles project.
-git submodule foreach git pull origin master
+#git submodule foreach git pull origin master
+git submodule foreach --recursive 'branch="$(git config -f .gitmodules submodule.$name.branch)"; git checkout $branch; git pull --rebase $branch'
