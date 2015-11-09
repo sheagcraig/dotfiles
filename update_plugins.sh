468c3de10fa59cf75e0205de93eef85b46ca97dc
Shea Craig <sheagcraig@gmail.com>
2015-11-08 21:07:36 -0500

    Develop branch broken-reverting to master.

--- update_plugins.sh
+++ update_plugins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Update vim submodules for updating the git dotfiles project.
 #git submodule foreach git pull origin master
-git submodule foreach --recursive 'branch="$(git config -f .gitmodules submodule.$name.branch)"; git checkout $branch; git pull --rebase $branch'
+#git submodule foreach --recursive 'branch="$(git config -f .gitmodules submodule.$name.branch)"; git checkout $branch; git pull --rebase $branch'
+git submodule foreach --recursive 'branch="$(git config -f .gitmodules submodule.$name.branch)"; git pull origin $branch'
