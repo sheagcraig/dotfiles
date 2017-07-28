e322af4b9dccd3e853c7ee2b0d0dbccbe8cdaf67
Shea Craig <sheagcraig@gmail.com>
2017-07-27 21:24:46 -0400

    Update submodules

--- update_plugins.sh
+++ update_plugins.sh
@@ -2,4 +2,5 @@
 # Update vim submodules for updating the git dotfiles project.
 #git submodule foreach git pull origin master
 #git submodule foreach --recursive 'branch="$(git config -f .gitmodules submodule.$name.branch)"; git checkout $branch; git pull --rebase $branch'
-git submodule foreach --recursive 'branch="$(git config -f .gitmodules submodule.$name.branch)"; git pull origin $branch'
+# git submodule foreach --recursive 'branch="$(git config -f .gitmodules submodule.$name.branch)"; git pull origin $branch'
+git submodule update --remote
