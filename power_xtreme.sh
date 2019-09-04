b046858d4f0bc4d547df6ab7ca66653f6e54972b
Shea Craig <sheagcraig@gmail.com>
2019-09-04 16:02:13 -0400

    Install brew as part of power extreme run. Use full path to salt-call.

--- power_xtreme.sh
+++ power_xtreme.sh
@@ -1,10 +1,15 @@
-# sudo salt-call \
-# 	--config-dir=${PWD} \
-# 	-l debug \
-# 	saltutil.sync_modules 
+if [[ ! $(which brew) ]]; then
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-sudo salt-call \
+sudo /opt/salt/bin/salt-call \
 	--config-dir=${PWD} \
 	-l debug \
-	state.highstate \
+	saltutil.sync_modules
+
+sudo /opt/salt/bin/salt-call \
+	--config-dir=${PWD} \
+	-l debug \
+	state.apply \
 	pillar="{'user': '$USER', 'home': '$HOME', 'secrets_dir': '$HOME/Developer/dotfiles/secrets'}" 
+
